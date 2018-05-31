7bc61e7564470b483069ba8e0b297a21df7a7bc8
Rodion Kochetkov <www.radik@yandex.by>
2018-06-01 00:52:59 +0300

    i have add some function for help with working with strings

--- thirdLesson_Methods/script.js
+++ thirdLesson_Methods/script.js
@@ -5,4 +5,28 @@
 const cout = (str) => str.length;
 
 //2
-const ()
+
+const pow = (x, n) => {
+
+	if(n == 1){
+    	return x;
+    }else{
+    	return x*pow(x, n-1);
+	}
+};
+
+const powX = (x, n) => {
+
+	x = Math.round(x);
+	n = Math.round(n);
+
+	return pow(x, n);
+};
+
+//3
+const checkingComb = (str, comb) => {
+
+	let reg = new RegExp (`${comb}`, "ig");
+
+	return !!(str.match(reg));
+};
