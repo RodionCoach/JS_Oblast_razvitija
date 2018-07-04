3b5cb4344b2e45bd7d7995c279974d181af0cf1b
Rodion Kochetkov <www.radik@yandex.by>
2018-07-04 17:22:52 +0300

    modified first task in script.js

--- seventhLesson_Conversion/script.js
+++ seventhLesson_Conversion/script.js
@@ -24,7 +24,30 @@ mul.value = 1;
 
 console.log(mul(3)(4)(5)(""));
 
-//2
+//1.3
+function sum(a) {
+
+    var currentSum = a;
+
+    function f(b) {
+        currentSum += b;
+        return f;
+    }
+
+    f.toString = function() {
+        return currentSum;
+    };
+
+    return f;
+}
+
+alert( sum(1)(2) ); // 3
+alert( sum(5)(-1)(2) ); // 6
+alert( sum(6)(-1)(-2)(-3) ); // 0
+alert( sum(0)(1)(2)(3)(4)(5) ); // 15
+
+//-----------------------------------------------------------------------------------------------------------------------
+// 2
 
 const sumStringArgs = function(){
 
